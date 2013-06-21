d23031279f6de228956e14443d9d7617183b14db
Ankush Shah <shah@Param.(none)>
2013-06-21 23:04:20 +0200

    project structure

---
+++ scripts/crawler.py
@@ -0,0 +1,12 @@
+'''
+Created on Jun 21, 2013
+
+@author: shah
+@email: ankush.shah.nitk@gmail.com
+
+Description: A crawler to extract all structure or meaningful text from a website. 
+
+'''
+
+if __name__ == '__main__':
+    pass
